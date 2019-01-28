d1ebd35aace0fc2edfe5353ce595e3c3d508cb95
iampersistent <shank@zestic.com>
2019-01-28 09:14:24 -0500

    set up container

---
+++ src/Context/Initializer/ApiClientAwareInitializer.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace Xaddax\GraphQLExtension\Context\Initializer;
+
+final class ApiClientAwareInitializer
+{
+}
---
+++ src/ServiceContainer/GraphQLExtention.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace Xaddax\GraphQLExtension\ServiceContainer;
+
+use Behat\Behat\Context\ServiceContainer\ContextExtension;
+use Behat\Testwork\ServiceContainer\Extension as ExtensionInterface;
+use Behat\Testwork\ServiceContainer\ExtensionManager;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+final class GraphQLExtention implements ExtensionInterface
+{
+    const CLIENT_ID = 'graphql.client';
+
+    public function getConfigKey()
+    {
+        return 'graphql';
+    }
+
+    public function initialize(ExtensionManager $extensionManager)
+    {
+        // TODO: Implement initialize() method.
+    }
+
+    public function configure(ArrayNodeDefinition $builder)
+    {
+        $builder
+            ->children()
+                ->arrayNode('apiClientOptions')
+                    ->addDefaultsIfNotSet()
+                    ->ignoreExtraKeys(false)
+                    ->end()
+                ->scalarNode('baseUrl')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->defaultValue('http://localhost:8080')
+                    ->end()
+                ->scalarNode('path')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->defaultValue('graphql')
+                    ->end()
+                ->scalarNode('method')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->defaultValue('POST')
+                    ->end()
+            ->end();
+    }
+
+    public function load(ContainerBuilder $container, array $config)
+    {
+        $this->loadClient($container, $config);
+        $this->loadContextInitializer($container, $config);
+    }
+
+    private function loadClient(ContainerBuilder $container, $config)
+    {
+        $guzzleClient = array_merge($config['apiClientOptions'], ['base_uri' => $config['baseUrl']]);
+        $definition = new Definition('GuzzleHttp\Client', [$guzzleClient]);
+        $container->setDefinition(self::CLIENT_ID, $definition);
+    }
+
+    private function loadContextInitializer(ContainerBuilder $container, $config)
+    {
+        $definition = new Definition(
+            'Xaddax\GraphqlExtension\Context\Initializer\ApiClientAwareInitializer', [
+            new Reference(self::CLIENT_ID),
+            $config,
+        ]
+        );
+        $definition->addTag(ContextExtension::INITIALIZER_TAG);
+        $container->setDefinition('graphql.context_initializer', $definition);
+    }
+}
